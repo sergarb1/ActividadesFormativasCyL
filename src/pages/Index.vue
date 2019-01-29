c3f6e1d186eca551a4079a770f764002d0041634
sergarb1 <sergi.profesor@gmail.com>
2019-01-29 23:11:55 +0100

    Ya obtenemos JSON

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,39 +1,26 @@
 <template>
   <q-page class="flex flex-center">
-    <q-list>
-    <!-- Elemento colapsable con Estaciones ValenBisi -->
-      <q-item class="justify-center">
-        <!-- Elemento colapsable https://quasar-framework.org/components/collapsible.html -->
-        <q-collapsible icon="account_balance" label="Nombres de cursos" opened class="col-lg" >
-          <q-list highlight separator >
-            <q-list-header>Datos actualizados {{ ultimaActualizacion }}
-
-              <!-- Boton que actualiza los datos de ValenBisi, que se ve como un iconito
+    <!-- Elemento colapsable https://quasar-framework.org/components/collapsible.html -->
+    <q-collapsible icon="account_balance" label="Actividades CyL" opened class="col-lg">
+      <q-list highlight separator>
+        <q-list-header>
+          Datos actualizados {{ ultimaActualizacion }}
+          <!-- Boton que actualiza los datos de ValenBisi, que se ve como un iconito
               https://quasar-framework.org/components/button.html
-              -->
-              <q-btn @click="getEstadoActividades" icon="update" />
-            </q-list-header>
-            <q-item>
-              <!-- Usamos el componente q-table
+          -->
+          <q-btn @click="getEstadoActividades" icon="update"/>
+        </q-list-header>
+      </q-list>
+
+      <!-- Usamos el componente q-table
               En el definimos:
                 - Fuente de datos
                 - Como organizamos las columnas
                 - Key (campo con valor unico)
                 https://quasar-framework.org/components/datatable.html
-              -->
-              <q-table
-                :data="estaciones"
-                :columns="columns"
-                row-key="nombre"
-                 class="col-lg"
-              >
-
-              </q-table>
-            </q-item>
-          </q-list>
-        </q-collapsible>
-      </q-item>
-      </q-list>
+      -->
+      <q-table :data="actividades" :columns="columns" class="col-lg"></q-table>
+    </q-collapsible>
   </q-page>
 </template>
 
@@ -52,28 +39,26 @@ export default {
   data() {
     return {
       // URL para obtener datos JSON de ValenBisi
-      endpoint: "https://admin.sigecyl.es/servicios/actividades/actividadesPresenciales?tipoActividad=charla&tipoActividad=curso&tipoActividad=taller&centro=leon&numActividades=3&fechaInicio=01/10/2013&fechaFin=20/11/2014",
+      endpoint:
+        "https://admin.sigecyl.es/servicios/actividades/actividadesPresenciales?tipoActividad=charla&tipoActividad=curso&tipoActividad=taller&centro=leon",
       // Array con información de cada uno de las estacione
-      estaciones: [],
+      actividades: [],
       // Fecha de ultima actualizacion en formato Cadena
-      ultimaActualizacion:'',
+      ultimaActualizacion: "",
 
       // Array que define las columnas de la tabla
       columns: [
-      {
-        name: 'Nombre',
-        required: true,
-        label: 'Nombre estación',
-        align: 'left',
-        field: 'nombre',
-        sortable: true,
-        //classes: '',
-        //style: 'width: 500px'
-      }
-
-    ]
-
-
+        {
+          name: "Nombre",
+          required: true,
+          label: "Nombre actividad",
+          align: "left",
+          field: "nombre",
+          sortable: true
+          //classes: '',
+          //style: 'width: 500px'
+        }
+      ]
     };
   },
   // Acciones al realizar al acabar de montarse Vue en el componente
@@ -83,32 +68,33 @@ export default {
   // Metodos accesibles desde Vue
   methods: {
     // Funcion que mediante axios, obtiene el estado del ValenBisi y rellena el array Estaciones
-    getEstadoActividades() {  
+    getEstadoActividades() {
       // Para que nos devuelvan los datos en JSON
-      axios.defaults.headers={
-        'Accept':'application/json'
-        };
+      axios.defaults.headers = {
+        Accept: "application/json"
+      };
       // Definimos el comportamiento de Axios
-      axios.get(this.endpoint)
+      axios
+        .get(this.endpoint)
         .then(response => {
-          console.log(response.data);
+          // Si, para coger este JSON debo hacer esta pirula
+          var miJson = JSON.parse(JSON.stringify(response.data));
+
           var x;
           // Vaciamos el array
-          this.estaciones=[];
-          // Pasamos el contenido al array "estaciones"
-          for (x in response.data.wsRespuestaActividadesPresenciales.actividades) {
+          this.actividades = [];
+          // Pasamos el contenido al array "actividades"
+          for (x in miJson.actividades) {
             //Formamos el dato
             var dato;
-
-            dato={
-              nombre: response.data.wsRespuestaActividadesPresenciales.actividades[x].nombre,
-            }
+            dato = {
+              nombre: miJson.actividades[x].nombre
+            };
             // Lo metemos en un array
-            this.estaciones.push(dato);
+            this.actividades.push(dato);
           }
           // Actualizamos la ultima fecha de actualizacion
-          this.ultimaActualizacion=new Date().toLocaleString();
-
+          this.ultimaActualizacion = new Date().toLocaleString();
         })
         // En caso de error, mostramos el error para facilitar depuración
         .catch(error => {
